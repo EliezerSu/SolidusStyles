dcbe2df389fa6d1d1f432bd4bf2b1fc6ac43076b
EliezerSu <eliezersuarez2000@gmail.com>
2023-11-29 18:23:20 -0500

    script

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('toggle-btn');
+    const toggleText = document.getElementById('toggle-text');
+  
+    toggleButton.addEventListener('change', function() {
+      if (this.checked) {
+        toggleText.innerText = 'Activado';
+      } else {
+        toggleText.innerText = 'Desactivado';
+      }
+    });
+  });
+  
