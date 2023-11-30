676b8dd4c42f1e92d784af87bd1c97b9da3ebc40
Eliezer suarez <eliezer.suarez@iguanadigital.com.ec>
2023-11-30 08:21:02 -0500

    Actualizando script

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('toggle-btn');
-    const toggleText = document.getElementById('toggle-text');
-  
-    toggleButton.addEventListener('change', function() {
-      if (this.checked) {
-        toggleText.innerText = 'Activado';
-      } else {
-        toggleText.innerText = 'Desactivado';
-      }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggle-btn");
+  const toggleText = document.getElementById("toggle-text");
+
+  toggleButton.addEventListener("change", function () {
+    if (this.checked) {
+      toggleText.innerText = "Activado";
+    } else {
+      toggleText.innerText = "Desactivado";
+    }
   });
-  
+});
+
+function copyText(text) {
+  navigator.clipboard.writeText(text);
+}
